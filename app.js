1816ffadaf29390d0f872b30df3a3e5ecbfbc591
AbidaAli100 <abida.ali100@gmail.com>
2021-07-31 11:56:50 +0500

    worked on the functionality

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-
-
 function convert() {
     let rupees = parseInt(document.querySelector('input').value);
+    if (document.querySelector('#unit').value == "D") {
 
-    if (document.querySelector('#dollar').value == "D") {
         let dollars = Math.round(rupees / 156.53);
-        console.log(dollars);
+        console.log(dollars)
         document.querySelector('span').innerHTML = "The amount in Dollar is " + dollars;
-    }
-    else if (document.querySelector('#saudiRiyal').value == "R") {
+    } else if (document.querySelector('#unit').value == "R") {
         let riyals = Math.round(rupees / 41.83);
-        console.log(riyals);
+
         document.querySelector('span').innerHTML = "The amount in Saudi-Riyal is " + riyals;
-    }
-    else if (document.querySelector('#pound').value == "P") {
+    } else if (document.querySelector('#unit').value == "P") {
         let pounds = Math.round(rupees / 216.63);
-        console.log(pounds);
         document.querySelector('span').innerHTML = "The amount in Pounds is " + pounds;
     }
 
 }
-
